1d2acf1d47a73632d94b4170cae331c55edd0dbb
keyiflerolsun <keyiflerolsun@gmail.com>
2023-02-28 14:16:15 +0300

    🕊
    🕊

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from io         import open
 setup(
     # ? Genel Bilgiler
     name         = "SelSik",
-    version      = "0.1.7",
+    version      = "0.1.8",
     url          = "https://github.com/keyiflerolsun/SelSik",
     description  = "@KekikAkademi Selenium Kütüphanesi",
     keywords     = ["SelSik", "KekikAkademi", "keyiflerolsun"],
